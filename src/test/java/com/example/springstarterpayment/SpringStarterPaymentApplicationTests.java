a3804b1ba80027ffb2d23b369e8baf2843db7303
Zhusup Akylbek <jojomen0039@gmail.com>
2026-02-24 16:26:08 +0600

    Initial Commit.

---
+++ src/test/java/com/example/springstarterpayment/SpringStarterPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstarterpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStarterPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
